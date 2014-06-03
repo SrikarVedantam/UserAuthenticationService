26d5d04f664930313b13a04d8f877f624ec7df4b
Alex Theedom <alextheedom@e-scape.co.uk>
2014-06-03 19:47:41 +0100

    Changes names of queues and exchanges.

--- UserAuthenticationService/src/main/java/co/uk/escape/UserAuthenticationConfiguration.java
+++ UserAuthenticationService/src/main/java/co/uk/escape/UserAuthenticationConfiguration.java
@@ -1,16 +1,18 @@
 package co.uk.escape;
 
+import static co.uk.escape.RMQExchange.Type.*;
+import static co.uk.escape.RMQQueue.Type.*;
+import static co.uk.escape.RMQTemplate.Type.*;
+
 import java.io.IOException;
 
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -20,48 +22,92 @@ import org.springframework.context.annotation.Configuration;
 import co.uk.escape.service.AuthenticateUserService;
 
 
-
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
 public class UserAuthenticationConfiguration {
 	
-    
-    @Bean
-    Queue requestQueue() {
-        return new Queue("requestqueue", false);
-    }
-    
-    
-    @Bean
-    DirectExchange exchange() {
-        return new DirectExchange("requestexchange");
-    }
-    
-        
+	
+	// QUEUES //	
+	@Bean @RMQQueue(LOGIN_RESPONSE)
+	Queue loginResponseQueue() {
+		return new Queue("LoginResponseQueue", true);
+	}
+
+	@Bean @RMQQueue(LOGIN_REQUEST)
+	Queue loginRequestQueue() {
+		return new Queue("LoginRequestQueue", true);
+	}	
+
+	@Bean @RMQQueue(REGISTRATION_REQUEST)
+	Queue registrationRequestQueue() {
+		return new Queue("RegistrationRequestQueue", true);
+	}
+	
+	@Bean @RMQQueue(REGISTRATION_RESPONSE)
+	Queue registrationResponseQueue() {
+		return new Queue("RegistrationResponseQueue", true);
+	}
+	
+
+	// EXCHANGE //
+	@Bean @RMQExchange(AUTHORISATION)
+	TopicExchange authorisationExchange() {
+		return new TopicExchange("AuthorisationExchange");
+	}
+	
+	@Bean @RMQExchange(RESPONSE)
+	TopicExchange responseExchange() {
+		return new TopicExchange("ResponseExchange");
+	}
+	
+	@Bean @RMQExchange(MESSAGE)
+	TopicExchange messageeExchange() {
+		return new TopicExchange("MessageExchange");
+	}
+	
+	
+	// BINDINGS //	
     @Bean
     public Binding binding() {
-        return BindingBuilder.bind(requestQueue()).to(exchange()).with("requestroutingkey");
+        return BindingBuilder.bind(loginRequestQueue()).to(authorisationExchange()).with("AuthorisationRoutingKey");
     }
     
-    
-    @Bean
-    RabbitTemplate template(DirectExchange exchange, ConnectionFactory connectionFactory){
+	@Bean
+	Binding replyBind(){
+		return BindingBuilder.bind(loginResponseQueue()).to(responseExchange()).with("ResponseRoutingKey");
+	}
+
+	@Bean
+	Binding registrationBind(){
+		return BindingBuilder.bind(registrationRequestQueue()).to(authorisationExchange()).with("RegistrationRoutingKey");
+	}
+	 
+        
+
+    /////////////////
+	// login user //
+    ////////////////
+	@Bean @RMQTemplate(LOGIN_USER)
+    RabbitTemplate loginTemplate(ConnectionFactory connectionFactory, 
+    		@RMQQueue(LOGIN_RESPONSE) Queue loginResponseQueue,
+    		@RMQExchange(RESPONSE) TopicExchange responseExchange){
         Jackson2JsonMessageConverter jsonConverter = new Jackson2JsonMessageConverter();
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);    
         rabbitTemplate.setMessageConverter(jsonConverter);
-        rabbitTemplate.setExchange("replyexchange");
-        rabbitTemplate.setQueue("replyqueue");
-        rabbitTemplate.setRoutingKey("replyroutingkey");
+        rabbitTemplate.setExchange(responseExchange.getName());
+        rabbitTemplate.setQueue(loginResponseQueue.getName());
+        rabbitTemplate.setRoutingKey("ResponseRoutingKey");
         return rabbitTemplate;
     }
     
     
 	@Bean
-	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory, AuthenticateUserService receiver, Queue authorisationRequestQueue) throws IOException {			
+	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory, AuthenticateUserService receiver, 
+			@RMQQueue(LOGIN_REQUEST) Queue loginRequestQueue) throws IOException {			
 		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
 		container.setConnectionFactory(connectionFactory);
-		container.setQueues(new Queue("requestqueue", false));
+		container.setQueues(loginRequestQueue);
 		container.setMessageListener(receiver);
 		return container;
 	}
@@ -70,41 +116,38 @@ public class UserAuthenticationConfiguration {
 	AuthenticateUserService receiver() {
 		return new AuthenticateUserService();
 	}  
-
+  
     
-    
-    
-    
-    
-    
-//	
-//	@Bean
-//	RabbitTemplate template(TopicExchange responseExchange, Queue responseQueue, ConnectionFactory connectionFactory) {
-//		Jackson2JsonMessageConverter jsonConverter = new Jackson2JsonMessageConverter();
-//		RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-//		rabbitTemplate.setMessageConverter(jsonConverter);
-//		rabbitTemplate.setQueue(responseQueue.getName());
-//		rabbitTemplate.setRoutingKey("authenticate");
-//		rabbitTemplate.setExchange(responseExchange.getName());
-//		return rabbitTemplate;
-//	}
-//	
-//
-//	
-//	
-//	@Bean
-//	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory, AuthenticateUserService receiver, Queue authorisationRequestQueue) throws IOException {			
-//		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-//		container.setConnectionFactory(connectionFactory);
-//		container.setQueues(authorisationRequestQueue);
-//		container.setMessageListener(receiver);
-//		return container;
-//	}
-//
-//	@Bean
-//	AuthenticateUserService receiver() {
-//		return new AuthenticateUserService();
-//	}
+	
+    ///////////////////////
+	// Register New User //
+    ///////////////////////
+	@Bean @RMQTemplate(REGISTER_USER)
+	RabbitTemplate registerTemplate(ConnectionFactory connectionFactory,
+			@RMQQueue(REGISTRATION_REQUEST) Queue registrationRequestQueue,
+			@RMQQueue(REGISTRATION_RESPONSE) Queue registrationResponseQueue,
+			@RMQExchange(AUTHORISATION) TopicExchange authorisationExchange){
+		Jackson2JsonMessageConverter jsonConverter = new Jackson2JsonMessageConverter();
+		RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+		rabbitTemplate.setMessageConverter(jsonConverter);
+		rabbitTemplate.setExchange(authorisationExchange.getName());
+		rabbitTemplate.setQueue(registrationRequestQueue.getName());
+		rabbitTemplate.setRoutingKey("RegistrationRoutingKey");
+		rabbitTemplate.setReplyQueue(registrationResponseQueue);
+		return rabbitTemplate;
+	}
+	
+    @Bean
+    public SimpleMessageListenerContainer replyListenerContainer(ConnectionFactory connectionFactory,
+    		@RMQTemplate(REGISTER_USER) RabbitTemplate registerTemplate,
+    		@RMQQueue(REGISTRATION_RESPONSE) Queue registrationResponseQueue) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueues(registrationResponseQueue);
+        container.setMessageListener(registerTemplate);
+        container.setReceiveTimeout(200000);
+        return container;
+    }
 	
 	
 	
--- UserAuthenticationService/src/main/java/co/uk/escape/service/AuthenticateUserService.java
+++ UserAuthenticationService/src/main/java/co/uk/escape/service/AuthenticateUserService.java
@@ -1,67 +1,127 @@
 package co.uk.escape.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageListener;
 import org.springframework.amqp.core.MessagePostProcessor;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.core.ChannelAwareMessageListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.support.CorrelationData;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-import co.uk.escape.domain.LoginRequest;
+import com.rabbitmq.client.Channel;
+
+import co.uk.escape.RMQTemplate;
 import co.uk.escape.domain.LoginResponse;
+import co.uk.escape.domain.LoginResponseMessageBundle;
 
 @Controller
-public class AuthenticateUserService implements MessageListener {
+public class AuthenticateUserService implements ChannelAwareMessageListener {	
 	
-	@Autowired
-	RabbitTemplate template;
-
-//	@Autowired
-//	TopicExchange responseExchange;		
+//	@Autowired @RMQTemplate(RMQTemplate.Type.REGISTER_USER)
+//	RabbitTemplate registerTemplate;
+	
+	@Autowired @RMQTemplate(RMQTemplate.Type.LOGIN_USER)
+	RabbitTemplate loginTemplate;
 	
 	@Override
-    public void onMessage(Message message) {
-
-        System.out.println("Message: " + message);      
-        System.out.println("Message: " + message.getMessageProperties().getCorrelationId());
-               
+	public void onMessage(Message message, Channel channel) throws Exception {
+		
+		RabbitTemplate template = null;
+		MessagePostProcessor messagePostProcessor = null;
+		List<String> permissions = new ArrayList<>();
+		LoginResponseMessageBundle loginResponseMessageBundle = null;
+		LoginResponse loginResponse = null;
+		String messageType = message.getMessageProperties().getHeaders().get("message-type").toString();
+		            
         Jackson2JsonMessageConverter jmc = new Jackson2JsonMessageConverter();
         Object messageObject = jmc.fromMessage(message);
+       
+        // START DEBUG //
+        System.out.println("Message: " + message);      
+        System.out.println("CorrelationId: " + message.getMessageProperties().getCorrelationId().toString());
+        System.out.println("messageType: " + messageType);       
+		System.out.println("Enter Authentication Service: " + messageObject);
+        // END DEBUG //       
+		
+        switch(messageType){
+        	case "login-request":    	
+	        	// TODO: Extract credentials from message and authenticate user.
+	    		// TODO: If login request is authenticated, send back authentication OK response, otherwise NOK
+	        	
+	    		// Authenticate user and respond with a LoginResponse object
+	    		loginResponse = new LoginResponse();
+	    		loginResponse.setAuthorised(true); // Lets authenticate user
+		
+	    		// Get correlation id so that the response message goes back to the correct sender.
+	    		final byte[] correlationId = message.getMessageProperties().getCorrelationId();
+	    		messagePostProcessor = new MessagePostProcessor() 
+	    				{
+	    			   		public Message postProcessMessage(Message message) throws AmqpException {
+	    			   			message.getMessageProperties().setCorrelationId(correlationId);
+	    			   		
+	    			   			return message;  
+	    			   } 
+	    		};
+	    		
+	    		
+	    		// Add permissions to message bundle based on user 'role'
+	    		permissions.add("permission1");
+	    		permissions.add("permission2");
+	    		// Transform message payload into message payload
+	    		
+	    		//MessageBundle mb = (MessageBundle)messageObject;
+	    		//LoginRequest loginRequest = (LoginRequest) mb.getPayload();
+	    		
+	    		
+	    		//messageBundle = bundleMessage(loginRequest, permissions);
+	    		loginResponseMessageBundle = bundleMessage(loginResponse, permissions);
+	        	     	
+	    		// Select rabbit template
+	    		template = loginTemplate;
+	    		
+        	break;
+        case "add-user-request":
+        	// code
+        	
+        	//template = registerTemplate;
+        	break;      
+        default:
+        	// code
+        	break;
+        }
+        
+		System.out.println("loginResponseMessageBundle: " + loginResponseMessageBundle);
       
-		// Authenticate user and respond with a LoginResponse object
-		LoginResponse loginResponse = new LoginResponse();
-		loginResponse.setAuthorised(true);
-		     
-		System.out.println("Enter Authentication Service: " + (LoginRequest)messageObject);
 		
-		final byte[] correlationId = message.getMessageProperties().getCorrelationId();
-		//CorrelationData correlationData = new CorrelationData(correlationId.toString());
-		//System.out.print("correlationData: " + correlationData);
+		template.convertAndSend(loginResponseMessageBundle, messagePostProcessor);	
 		
-		MessagePostProcessor messagePostProcessor = new MessagePostProcessor() 
-				{
-			   		public Message postProcessMessage(Message message) throws AmqpException {
-			   			message.getMessageProperties().setCorrelationId(correlationId);;
-			   			return message;  
-			   } 
-		};
+		//template.convertAndSend(loginResponse, messagePostProcessor);
 		
 		
-		//template.convertAndSend("replyexchange", "replyroutingkey", loginResponse, messagePostProcessor);		
-		template.convertAndSend(loginResponse, messagePostProcessor);		
-		
-		System.out.println("UUID " + template.getUUID());		
-		System.out.println("Exit Authentication Service: " + loginResponse);	
+
+		// TODO: If not a login request and authentication OK put authenticated message on message queue. Ensure that correlationID is included.
+	
+		System.out.println("Exit Authentication Service: ");	
         
     }
 	
 	
 	
-	
+	// Bundle message
+	private LoginResponseMessageBundle bundleMessage(LoginResponse payload, List<String> permissions) {
+		LoginResponseMessageBundle messageBundle = new LoginResponseMessageBundle(payload);
+		//messageBundle.setPermissions(permissions);
+		return messageBundle;		
+	}
+
+
+
+
 	
 	
 	
